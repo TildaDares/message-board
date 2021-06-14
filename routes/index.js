e336ca0459eff160d3eac332abb01cf4b9f93985
TildaDares <mathildaudufo@gmail.com>
2021-06-14 11:04:46 +0100

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Tilda",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini MessageBoard", messages: messages });
 });
+
+router
+  .get("/new", function (req, res, next) {
+    res.render("form", { title: "New Message" });
+  })
+  .post("/new", function (req, res, next) {
+    const { message, author } = req.body;
+    messages.unshift({ text: message, user: author, added: new Date() });
+    res.redirect("/");
+  });
 
 module.exports = router;
